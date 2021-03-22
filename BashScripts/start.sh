896b71ea658a2efa3f5c66ccc8e80947c5ed9cdf
Trevor Ryan <77417269+ryantd2021@users.noreply.github.com>
2021-03-22 09:33:52 -0400

    Proxmox Bash Scripts
    Start, stop, and rollback bash scripts for Proxmox VMs added

---
+++ BashScripts/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+decodeDataFromJson(){
+    echo `echo $1 \
+            | sed 's/{\"data\"\:{//g' \
+            | sed 's/\\\\\//\//g' \
+            | sed 's/[{}]//g' \
+            | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
+            | sed 's/\"\:\"/\|/g' \
+            | sed 's/[\,]/ /g' \
+            | sed 's/\"// g' \
+            | grep -w $2 \
+            | awk -F "|" '{print $2}'`
+}
+PROX_USERNAME="root@pam"
+PROX_PASSWORD="G6601007r"
+HOST="https://172.16.66.10:8006"
+
+DATA=`curl -s -k -d "username=$PROX_USERNAME&password=$PROX_PASSWORD" $HOST/api2/json/access/ticket` 
+TICKET=$(decodeDataFromJson $DATA 'ticket')
+CSRF=$(decodeDataFromJson $DATA 'CSRFPreventionToken')
+
+NODE="pve"
+TARGET_VMID="100"
+
+START_TASK_DATA=`curl -s -k -b "PVEAuthCookie=$TICKET" -H "CSRFPreventionToken: $CSRF" -X POST $HOST/api2/json/nodes/$NODE/qemu/$TARGET_VMID/status/start`
+
+START_TASK_RESULT=$(decodeDataFromJson $START_TASK_DATA 'data')
