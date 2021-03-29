5bbfac2d91ac467036ce893d08423d8c698d0a5b
Jacob Sanford <50007949+sanforjr2021@users.noreply.github.com>
2021-03-29 16:32:03 -0400

    Created Web Access -Added tomcat support for an empty page.

---
+++ src/main/java/solarprox/web/Login.java
@@ -0,0 +1,35 @@
+package solarprox.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", value = "/MyServlet")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>MyServlet.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>This is a simple java servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sanford-2020
+  Date: 3/29/2021
+  Time: 4:26 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Hello, I am a Java web app!</title>
+</head>
+<body>
+<h1>Simple Java Web App Demo</h1>
+<p>To invoke the java servlet click <a href="MyServlet">here</a></p>
+</body>
+</html>
